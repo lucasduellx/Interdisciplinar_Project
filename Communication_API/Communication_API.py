a3e133b8f807e0e18f3985326a44c493d78625a8
Claus Alberto <claus.a.bienemann@gmail.com>
2022-04-27 11:37:31 -0300

    SQLite Implemented

--- Communication_API/Communication_API.py
+++ Communication_API/Communication_API.py
@@ -1,35 +1,79 @@
 # encoding: utf-8
 import json
-from flask import Flask, request, jsonify
-
+from flask import Flask, request, jsonify, g
+import sqlite3
 app = Flask(__name__)
+
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/", methods=["GET", "POST"])
 def starting_url():
+    conn = get_db()
+    cur = conn.cursor()
     ask = dict(json.loads(request.data))
-    answer = None
+    answer = {"answer":[]}
+    query = None
+    idList = []
 
     match request.method:
         case 'GET':
-            answer = {"answer":[]}
-
-            temperature = None
-            
             for item in ask['ask']:
                 print(item['id'])
-                temperature = item['id']
+                idList.append(item['id'])
+            
+            idList = str(idList).replace('[','(').replace(']',')')
+
+            query = cur.execute(f'SELECT Id, Temperature FROM Temperature WHERE Id IN {idList}')
+
+
+            for row in query:
                 answer['answer'].append(
                     {
-                        "id":item['id'],
-                        "temperature":temperature
+                        "id":row[0],
+                        "temperature":row[1]
                     }
                 )
+
         case 'POST':
             for item in ask['ask']:
-                print(f'id: {item["id"]}')
-                print(f'temperature: {item["temperature"]}')
-                print('------------------------')
+                if item["id"] == None:
+                    cur.execute(f'INSERT INTO Temperature (Temperature) VALUES ({item["temperature"]})')
+                    conn.commit()
+                    lastIdQuery = cur.execute(f'SELECT last_insert_rowid() FROM Temperature LIMIT 1')
+                    item["id"] = lastIdQuery.fetchone()[0]
 
+                else:
+                    cur.execute(f'UPDATE Temperature SET Temperature={item["temperature"]} WHERE Id = {item["id"]}')
+                    conn.commit()
+
+                idList.append(item["id"])
+
+            idList = str(idList).replace('[','(').replace(']',')')
+
+            query = cur.execute(f'SELECT Id, Temperature FROM Temperature WHERE Id IN {idList}')
+
+            for row in query:
+                answer['answer'].append(
+                {
+                    "id":row[0],
+                    "temperature":row[1]
+                }
+            )
+
+    cur.close()
+    conn.close()
     return jsonify(answer)
 
 
---
+++ Communication_API/Creation.sql
@@ -0,0 +1,9 @@
+-- SQLite
+
+CREATE TABLE Temperature(
+Id int AUTO_INCREMENT,
+Temp DOUBLE,
+PRIMARY KEY (Id)
+);
+
+SELECT * From Temperature
